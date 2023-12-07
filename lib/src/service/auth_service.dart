abfc237d863a04da9b8ec62fd29715345e1c2764
shmsacr <shmsacr@gmail.com>
2023-12-07 22:54:11 +0300

    feat:create auth service

--- lib/src/service/auth_service.dart
+++ lib/src/service/auth_service.dart
@@ -3,15 +3,14 @@ import 'dart:ffi';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-class AuthService{
+class AuthService {
   final DbCollection store;
   final String secret;
   AuthService({required this.store, required this.secret});
-  
-  Handle get router{
+
+  Handle get router {
     final app = Router();
-    app.post(, handler)
-    
+
     return app;
   }
 }
