ae9a727c17912de9cd73937c5ef9e5c497485a8c
shmsacr <shmsacr@gmail.com>
2023-12-08 22:50:01 +0300

    feat:create request User model

---
+++ lib/src/model/req_user.dart
@@ -0,0 +1,12 @@
+typedef UserType = Map<dynamic, dynamic>;
+
+class ReqUser {
+  final String? email;
+  final String? password;
+  const ReqUser({required this.email, required this.password});
+
+  factory ReqUser.fromJson(UserType json) =>
+      ReqUser(email: json['email'], password: json['password']);
+
+  UserType toJson() => {'email': email, 'password': password};
+}
