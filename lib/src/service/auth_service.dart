148db78293a34d1c456dcfe25ee03cd827cc4558
shmsacr <shmsacr@gmail.com>
2023-12-09 21:28:48 +0300

    feat:add register post request

--- lib/src/service/auth_service.dart
+++ lib/src/service/auth_service.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
 import 'dart:ffi';
+import 'dart:io';
 
+import 'package:dart_movie_api/src/extensions/parse_ext.dart';
 import 'package:mongo_dart/mongo_dart.dart';
+import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
+
+import '../extensions/http_ext.dart';
 
 class AuthService {
   final DbCollection store;
@@ -10,6 +16,16 @@ class AuthService {
 
   Handle get router {
     final app = Router();
+
+    /// Register POST Request
+    app.post("/register", (Request request) async {
+      final user = await request.parseData;
+      if (user.email!.isEmpty || user.password!.isEmpty) {
+        return Response(HttpStatus.badRequest,
+            body: jsonEncode({"error: Please provider your email/password"}),
+            headers: CustomHeader.json.getType);
+      }
+    });
 
     return app;
   }
