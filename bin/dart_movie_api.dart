ffb99d872dfe0cb9d9365cd25dbe33b0e06f0792
shmsacr <shmsacr@gmail.com>
2023-12-10 04:07:47 +0300

    feat:add app mount and provider register

--- bin/dart_movie_api.dart
+++ bin/dart_movie_api.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dart_movie_api/src/constants/app_constants.dart';
+import 'package:dart_movie_api/src/service/auth_service.dart';
 import 'package:dart_movie_api/src/service/db_service.dart';
+import 'package:dart_movie_api/src/service/password_service.dart';
 import 'package:dart_movie_api/src/service/provider.dart';
+import 'package:dart_movie_api/src/utils/email_validator.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -18,11 +22,22 @@ void main(List<String> arguments) async {
     return Response.ok(json.encode({'message': 'Hello World'}),
         headers: {'Content-Type': 'application/json'});
   });
-
+  app.mount(
+      '/auth',
+      AuthService(
+              store: Provider.of
+                  .fetch<DbService>()
+                  .getStore(AppConstants.userCollection),
+              secret: "123456")
+          .router);
   await serve(app, InternetAddress.anyIPv4, port);
   print('Server running on port $port');
 }
 
 Future<void> initData() async {
-  Provider.of.register(DbService, () => DbService());
+  Provider.of
+    ..register(DbService, () => DbService())
+    ..register(RegexValidator,
+        () => RegexValidator(regExpSource: AppConstants.emailRegex))
+    ..register(PasswordService, () => PasswordService());
 }
