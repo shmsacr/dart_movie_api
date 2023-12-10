6046c97f03a25531abc95bc23e9c3f2ded899f60
shmsacr <shmsacr@gmail.com>
2023-12-10 04:06:36 +0300

    feat:add register user and fix sytax

--- lib/src/service/auth_service.dart
+++ lib/src/service/auth_service.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'dart:io';
 
 import 'package:dart_movie_api/src/extensions/parse_ext.dart';
+import 'package:dart_movie_api/src/service/password_service.dart';
+import 'package:dart_movie_api/src/service/provider.dart';
+import 'package:dart_movie_api/src/utils/email_validator.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -14,8 +16,10 @@ class AuthService {
   final String secret;
   AuthService({required this.store, required this.secret});
 
-  Handle get router {
+  Handler get router {
     final app = Router();
+    final rx = Provider.of.fetch<RegexValidator>();
+    final ps = Provider.of.fetch<PasswordService>();
 
     /// Register POST Request
     app.post("/register", (Request request) async {
@@ -25,6 +29,25 @@ class AuthService {
             body: jsonEncode({"error: Please provider your email/password"}),
             headers: CustomHeader.json.getType);
       }
+      final addedUser = await store.findOne(where.eq("email", user.email));
+      if (addedUser != null) {
+        return Response.badRequest(
+            body: json.encode({"error": "User already exists"}),
+            headers: CustomHeader.json.getType);
+      }
+      if (!rx.isValid(user.email!)) {
+        return Response(HttpStatus.badRequest,
+            body:
+                json.encode({"error": "Please provide correct email address"}),
+            headers: CustomHeader.json.getType);
+      }
+      final salt = ps.generateSalt();
+      final hashedPassword = ps.hashPassword(user.password!, salt);
+      await store.insertOne(
+          {"email": user.email, 'password': hashedPassword, 'salt': salt});
+      return Response.ok(
+          json.encode({'message': 'User saved. Please redirec to login'}),
+          headers: CustomHeader.json.getType);
     });
 
     return app;
