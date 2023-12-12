84ff912b4770ad9406faa5d157c8a825e0105af8
shmsacr <shmsacr@gmail.com>
2023-12-12 22:44:25 +0300

    feat: add env , register dbservice and token service

--- bin/dart_movie_api.dart
+++ bin/dart_movie_api.dart
@@ -2,10 +2,13 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:dart_movie_api/src/constants/app_constants.dart';
+import 'package:dart_movie_api/src/model/token_secret.dart';
 import 'package:dart_movie_api/src/service/auth_service.dart';
 import 'package:dart_movie_api/src/service/db_service.dart';
 import 'package:dart_movie_api/src/service/password_service.dart';
 import 'package:dart_movie_api/src/service/provider.dart';
+import 'package:dart_movie_api/src/service/token_service.dart';
+import 'package:dart_movie_api/src/utils/config.dart';
 import 'package:dart_movie_api/src/utils/email_validator.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
@@ -14,7 +17,7 @@ import 'package:shelf_router/shelf_router.dart';
 void main(List<String> arguments) async {
   await initData();
   final app = Router();
-  final port = 8080;
+  final port = Env.port;
   final dbInstance = Provider.of.fetch<DbService>();
   await dbInstance.openDb();
 
@@ -30,7 +33,7 @@ void main(List<String> arguments) async {
                   .getStore(AppConstants.userCollection),
               secret: "123456")
           .router);
-  await serve(app, InternetAddress.anyIPv4, port);
+  await serve(app, InternetAddress.anyIPv4, int.parse(port));
   print('Server running on port $port');
 }
 
@@ -39,5 +42,12 @@ Future<void> initData() async {
     ..register(DbService, () => DbService())
     ..register(RegexValidator,
         () => RegexValidator(regExpSource: AppConstants.emailRegex))
-    ..register(PasswordService, () => PasswordService());
+    ..register(PasswordService, () => PasswordService())
+    ..register(
+        TokenService,
+        () => TokenService(
+            store: Provider.of
+                .fetch<DbService>()
+                .getStore(AppConstants.tokenCollection)))
+    ..register(TokenSecret, () => TokenSecret());
 }
