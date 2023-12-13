deed941c5f297920ef986e9fd73854c44929f17a
shmsacr <shmsacr@gmail.com>
2023-12-13 23:27:28 +0300

    feat: add create token method

--- lib/src/service/token_service.dart
+++ lib/src/service/token_service.dart
@@ -1,11 +1,14 @@
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:dart_movie_api/src/model/token_pair.dart';
+import 'package:dart_movie_api/src/model/token_secret.dart';
+import 'package:dart_movie_api/src/service/provider.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class TokenService {
   final DbCollection store;
   TokenService({required this.store});
   String generateJwt(String subject, String issuer, String secret,
-      [String? jwtId, Duration expiry = const Duration(minutes: 1)]) {
+      {String? jwtId, Duration expiry = const Duration(minutes: 1)}) {
     final jwt = JWT(
       {'iat': DateTime.now().millisecondsSinceEpoch},
       issuer: issuer,
@@ -24,5 +27,28 @@ class TokenService {
     } on JWTInvalidException {
       throw Exception("Invalid token...");
     }
+  }
+
+  Future<TokenPair> createTokenPair(String userId) async {
+    final ts = Provider.of.fetch<TokenSecret>();
+
+    final String tokenId = Uuid().v4();
+    final aToken =
+        generateJwt(userId, 'http://localhost', ts.getSecret, jwtId: tokenId);
+    const refresTokenExpiry = Duration(minutes: 5);
+    final rToken = generateJwt(userId, 'http://localhost', ts.getSecret,
+        expiry: refresTokenExpiry, jwtId: tokenId);
+    await addTokenPair(tokenId, aToken, rToken);
+    return TokenPair(aToken: aToken, rToken: rToken);
+  }
+
+  Future<void> addTokenPair(
+      String tokenId, String aToken, String rToken) async {
+    await store
+        .insertOne({'tokenId': tokenId, 'aToken': aToken, 'rToken': rToken});
+  }
+
+  Future<dynamic> removeToken(String tokenId) async {
+    return await store.deleteOne({'tokenId': tokenId});
   }
 }
