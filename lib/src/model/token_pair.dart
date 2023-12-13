b52780011f1d84a722942e2a52d13311b0e8dc4e
shmsacr <shmsacr@gmail.com>
2023-12-13 23:26:18 +0300

    feat: creae token pair

---
+++ lib/src/model/token_pair.dart
@@ -0,0 +1,12 @@
+typedef TokenType = Map<String, dynamic>;
+
+class TokenPair {
+  final String aToken;
+  final String rToken;
+  const TokenPair({required this.aToken, required this.rToken});
+
+  TokenType toJson() => {
+        'accesToken': aToken,
+        'refreshToken': rToken,
+      };
+}
