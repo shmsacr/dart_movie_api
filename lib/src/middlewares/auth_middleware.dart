c61c8a264af7ff8ca8d58d80907700253bcde152
Sehmus Acar <shmsacr@gmail.com>
2023-12-15 22:48:13 +0300

    feat: create auth middleware and method

---
+++ lib/src/middlewares/auth_middleware.dart
@@ -0,0 +1,24 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:dart_movie_api/src/model/token_secret.dart';
+import 'package:dart_movie_api/src/service/token_service.dart';
+import 'package:shelf/shelf.dart';
+
+import '../service/provider.dart';
+
+Middleware authMiddleware() {
+  return (Handler innerHadler) {
+    return (Request request) {
+      final ts = Provider.of.fetch<TokenService>();
+      final autheader = request.headers['Authorization'];
+      final tsInst = Provider.of.fetch<TokenSecret>();
+      String? token;
+      JWT? jwt;
+      if (autheader != null && autheader.startsWith('Bearer ')) {
+        token = autheader.substring(7);
+        jwt = ts.verifyJwt(token, tsInst.getSecret);
+      }
+      final updateRequest = request.change(context: {'authDetails': jwt});
+      return innerHadler(updateRequest);
+    };
+  };
+}
